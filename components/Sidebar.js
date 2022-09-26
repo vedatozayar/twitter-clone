8c8e66f55a41b44526c96e65bcfe15c3014426cf
vedatozayar <vedatozayar@gmail.com>
2022-09-26 16:42:46 +0300

    first commit

---
+++ components/Sidebar.js
@@ -0,0 +1,52 @@
+import React from 'react';
+import Image from 'next/image';
+import SidebarLink from './SidebarLink';
+
+import {
+  HomeIcon,
+  HashtagIcon,
+  BellIcon,
+  InboxIcon,
+  BookmarkIcon,
+  ClipboardListIcon,
+  UserIcon,
+  DotsCircleHorizontalIcon,
+  DotsHorizontalIcon,
+} from '@heroicons/react/outline';
+
+function Sidebar() {
+  return (
+    <div className='hidden sm:flex flex-col items-center xl:items-start xl:w-[340px] p-2 fixed h-full'>
+      <div className='flex items-center justify-center w-14 h-14 hoverAnimation p-0 xl:ml-24'>
+        <Image src='https://rb.gy/ogau5a' width={30} height={30} />
+      </div>
+      <div className='space-y-2.5 mt-4 mb-2.5 xl:ml-24'>
+        <SidebarLink text='Home' Icon={HomeIcon} active />
+        <SidebarLink text='Explore' Icon={HashtagIcon} />
+        <SidebarLink text='Notifications' Icon={BellIcon} />
+        <SidebarLink text='Messages' Icon={InboxIcon} />
+        <SidebarLink text='Bookmarks' Icon={BookmarkIcon} />
+        <SidebarLink text='Lists' Icon={ClipboardListIcon} />
+        <SidebarLink text='Profile' Icon={UserIcon} />
+        <SidebarLink text='More' Icon={DotsCircleHorizontalIcon} />
+      </div>
+      <button className='hidden xl:inline ml-auto bg-[#1d9bf0] text-white rounded-full w-56 h-[52px] text-lg font-bold shadow-md hover:bg-[#1a8cd8]'>
+        Tweet
+      </button>
+      <div className='text-[#d9d9d9] flex items-center justify-center hoverAnimation xl:ml-auto xl:mr-5 mt-auto'>
+        <img
+          src='https://media-exp1.licdn.com/dms/image/C5603AQH0_ZArQNwFGQ/profile-displayphoto-shrink_400_400/0/1617141657757?e=1669852800&v=beta&t=EzPwyFlvoW6p65e1TTAj9TubbS1q4mULWnhZQStZe38'
+          alt=''
+          className='h-10 w-10 rounded-full xl:mr-2.5'
+        />
+        <div className='hidden xl:inline leading-5'>
+          <h4 className='font-bold'>Vedat 1875</h4>
+          <p className='text-[#6e767d] '>@vedat1875</p>
+        </div>
+        <DotsHorizontalIcon className='h-5 hidden xl:inline ml-10' />
+      </div>
+    </div>
+  );
+}
+
+export default Sidebar;
--- next.config.js
+++ next.config.js
@@ -2,6 +2,13 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+//module.exports = nextConfig;
+
+// next.config.js
+module.exports = {
+  images: {
+    domains: ['rb.gy'],
+  },
+};
